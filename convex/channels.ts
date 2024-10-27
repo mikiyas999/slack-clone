230b8f3c7d4550e0790a567455057d9851c1faa6
Mikiyas Getachew <mikiyasget1234@gmail.com>
2024-10-27 09:52:34 +0000

    create channe get endpoint

---
+++ convex/channels.ts
@@ -0,0 +1,30 @@
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+import { getAuthUserId } from "@convex-dev/auth/server";
+
+export const get = query({
+  args: {
+    workspaceId: v.id("workspaces"),
+  },
+  handler: async (ctx, { workspaceId }) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (userId === null) return [];
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_and_user_id", (q) =>
+        q.eq("workspaceId", workspaceId).eq("userId", userId)
+      )
+      .unique();
+
+    if (!member) return [];
+
+    const channels = await ctx.db
+      .query("channels")
+      .withIndex("by_workspace_id", (q) => q.eq("workspaceId", workspaceId))
+      .collect();
+
+    return channels;
+  },
+});
