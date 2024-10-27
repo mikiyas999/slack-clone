04644cffce6b1b39ea1bf9081f442cb0124b6df1
Mikiyas Getachew <mikiyasget1234@gmail.com>
2024-10-27 09:54:42 +0000

    add conversation, message and reaction table to schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -23,6 +23,42 @@ const schema = defineSchema({
     name: v.string(),
     workspaceId: v.id("workspaces"),
   }).index("by_workspace_id", ["workspaceId"]),
+  conversations: defineTable({
+    memberOneId: v.id("members"),
+    memberTwoId: v.id("members"),
+    workspaceId: v.id("workspaces"),
+  }).index("by_workspace_id", ["workspaceId"]),
+
+  messages: defineTable({
+    body: v.string(),
+    memberId: v.id("members"),
+    workspaceId: v.id("workspaces"),
+    updatedAt: v.optional(v.number()),
+    image: v.optional(v.id("_storage")),
+    channelId: v.optional(v.id("channels")),
+    parentMessageId: v.optional(v.id("messages")),
+    conversationId: v.optional(v.id("conversations")),
+  })
+    .index("by_member_id", ["memberId"])
+    .index("by_channel_id", ["channelId"])
+    .index("by_workspace_id", ["workspaceId"])
+    .index("by_conversation_id", ["conversationId"])
+    .index("by_parent_message_id", ["parentMessageId"])
+    .index("by_channel_id_parent_message_id_conversation_id", [
+      "channelId",
+      "parentMessageId",
+      "conversationId",
+    ]),
+
+  reactions: defineTable({
+    value: v.string(),
+    memberId: v.id("members"),
+    messageId: v.id("messages"),
+    workspaceId: v.id("workspaces"),
+  })
+    .index("by_member_id", ["memberId"])
+    .index("by_message_id", ["messageId"])
+    .index("by_workspace_id", ["workspaceId"]),
 });
 
 export default schema;
