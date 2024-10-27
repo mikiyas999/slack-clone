91bb6827ee3c38359756f832afd3819b01f5a82d
Mikiyas Getachew <mikiyasget1234@gmail.com>
2024-10-27 09:55:32 +0000

    add shadcn comp.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -25,6 +25,7 @@ const buttonVariants = cva(
         sm: "h-8 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
+        iconSm: "size-8",
       },
     },
     defaultVariants: {
